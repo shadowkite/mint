b2a19570861c1e2c7d49286326c6d8a31a2fc6ff
Leander Vink <leandersvink@gmail.com>
2021-06-05 13:51:25 +0200

    Fixed wallet session bug

--- public/index.php
+++ public/index.php
@@ -1,11 +1,11 @@
 <?php
 
-session_start();
 // if(!isset($_GET['testmode'])) { die("Down for maintenance, please try later."); }
 error_reporting(E_ALL ^ E_WARNING);
 // ini_set('display_errors', 'on');
 
 require_once __DIR__ . '/../vendor/autoload.php';
+session_start();
 $app = new \Application();
 $app->setControllerPath(__DIR__ . '/../src/Controllers');
 $app->bootstrap("view", function(){
